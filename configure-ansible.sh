eac2af75633e673231acc3f15dc4183054c4fa45
Andrés Peralta <ar.peralta.3@gmail.com>
2017-07-15 18:50:06 -0500

    Create configure-ansible.sh
    Create script to configure ansible on install machine

---
+++ configure-ansible.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+sudo pacman -S python openssh ansible
+
+git clone https://github.com/arperalta3/ansible-packer.git ~/.config/ansible-pacaur
+
+git clone https://github.com/arperalta3/dotfiles.git ~/.config/dotfiles
+
+ln -s ~/.config/ansible-pacaur/pacaur ~/.config/dotfiles/ansible-playbooks/library/pacaur
+
+cd ~/.config/dotfiles/
