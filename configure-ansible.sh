3f5267f5c835e597a51a5845d61ce4fafbdc4ab1
Andrés Peralta <ar.peralta.3@gmail.com>
2017-07-15 20:07:01 -0500

    Update configure-ansible.sh
    Add documentation to configure-ansible.sh

--- configure-ansible.sh
+++ configure-ansible.sh
@@ -1,11 +1,16 @@
 #! /bin/sh
 
+# Install packages python, openssh and ansible
 sudo pacman -S python openssh ansible
 
+# Clone ansible-packer repo
 git clone https://github.com/arperalta3/ansible-packer.git ~/.config/ansible-pacaur
 
+# Clone dotfiles repo
 git clone https://github.com/arperalta3/dotfiles.git ~/.config/dotfiles
 
+# Link ansible library pacaur
 ln -s ~/.config/ansible-pacaur/pacaur ~/.config/dotfiles/ansible-playbooks/library/pacaur
 
+# Set current directory to dotfiles repo
 cd ~/.config/dotfiles/
