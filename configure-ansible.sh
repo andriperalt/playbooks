ef6b75a745c12404114c22857cc1d400dc7f2b73
Andrés Peralta <ar.peralta.3@gmail.com>
2017-07-16 12:40:41 -0500

    add asnible callback plugin human log

--- configure-ansible.sh
+++ configure-ansible.sh
@@ -25,8 +25,15 @@ ssh-copy-id $USER@localhost
 printf "\n\nCloning ansible-pacaur repo\n\n"
 git clone https://github.com/arperalta3/ansible-pacaur.git ~/.config/ansible-pacaur
 
+printf "\n\nCloning ansible-human-log repo\n\n"
+git clone https://gist.github.com/cd706de198c85a8255f6.git ~/.config/ansible-human-log
+
 printf "\n\nCloning playbooks repo\n\n"
 git clone https://github.com/arperalta3/playbooks.git ~/.config/playbooks
 
 printf "\n\nLinking ansible library pacaur\n\n"
 ln -s ~/.config/ansible-pacaur/pacaur ~/.config/playbooks/ansible-playbooks/library/pacaur
+
+printf "\n\nLinking ansible callback human log\n\n"
+ln -s ~/.config/ansible-human-log/human_log.py 
+~/.config/playbooks/ansible-playbooks/callback_plugins/human_log.py
