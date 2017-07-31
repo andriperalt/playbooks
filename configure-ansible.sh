3c1a56f9e74c3c540bed1c751fd912af9c6415e6
Andrés Peralta <ar.peralta.3@gmail.com>
2017-07-31 00:39:54 -0500

    Exit on error on configure ansible script

--- configure-ansible.sh
+++ configure-ansible.sh
@@ -1,50 +1,65 @@
-#! /bin/sh
+#!/bin/bash
 
-printf "\n\nConfigure correct clock\n\n"
-sudo timedatectl set-ntp true
-sudo hwclock --systohc --utc
+# Prepare logs
+>configure-ansible.log
+>configure-ansible.err
+exec > >(tee -ia configure-ansible.log) 2> >(tee -ia configure-ansible.err >&2)
 
-printf "\n\nInstalling reflector\n\n"
-sudo pacman -S --needed reflector
+# Fail if variables unset
+set -o nounset
+# Fail if any error
+set -o errexit
 
-printf "\n\nRunning reflector\n\n"
-sudo reflector --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
-
-printf "\n\nInstalling packages gnome-keyring, git, python, openssh and ansible\n\n"
-sudo pacman -S --needed gnome-keyring libsecret seahorse git python openssh ansible
-
-printf "\n\nInstalling cower for pacaur\n\n"
-git clone https://aur.archlinux.org/cower.git ~/.config/cower
-gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
-cd ~/.config/cower && exec makepkg -si
-
-printf "\n\nInstalling pacaur\n\n"
-git clone https://aur.archlinux.org/pacaur.git ~/.config/pacaur
-cd ~/.config/pacaur && exec makepkg -si
-
-printf "\n\nStarting ssh service\n\n"
-sudo systemctl start sshd.socket
-
-printf "\n\nGenerating ssh-keygen\n\n"
-ssh-keygen -t rsa -b 4096
-
-printf "\n\nCopying ssh-key\n\n"
-ssh-copy-id $USER@localhost
-
-printf "\n\nRemove repos\n\n"
-rm -rf ~/.config/playbooks ~/.config/ansible-pacaur ~/.config/ansible-human-log
-
-printf "\n\nCloning ansible-pacaur repo\n\n"
-git clone https://github.com/arperalta3/ansible-pacaur.git ~/.config/ansible-pacaur
-
-printf "\n\nCloning ansible-human-log repo\n\n"
-git clone https://github.com/jinesh-choksi/ansible-human_log.git ~/.config/ansible-human-log
-
-printf "\n\nCloning playbooks repo\n\n"
-git clone https://github.com/arperalta3/playbooks.git ~/.config/playbooks
-
-printf "\n\nLinking ansible library pacaur\n\n"
-ln -s ~/.config/ansible-pacaur/pacaur ~/.config/playbooks/ansible-playbooks/library/pacaur
-
-printf "\n\nLinking ansible callback human log\n\n"
-ln -s ~/.config/ansible-human-log/human_log.py ~/.config/playbooks/ansible-playbooks/callback_plugins/human_log.py
+{
+  echo "====== INFO: Configuring correct clock ======" \
+  && sudo timedatectl set-ntp true \
+  && sudo hwclock --systohc --utc \
+  && echo "====== INFO: Configuring correct clock ======" \
+  && echo ""
+} && {
+  echo "====== INFO: Installing reflector ======" \
+  && sudo pacman -S --needed reflector \
+  && echo "====== INFO: Installed reflector ======" \
+} && {
+  echo "\n\nRunning reflector\n\n" \
+  && sudo reflector --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
+} && {
+  echo "\n\nInstalling packages gnome-keyring, git, python, openssh and ansible\n\n" \
+  && sudo pacman -S --needed gnome-keyring libsecret seahorse git python openssh ansible
+} && {
+  echo "\n\nInstalling cower for pacaur\n\n" \
+  && git clone https://aur.archlinux.org/cower.git ~/.config/cower \
+  && gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53 \
+  && { cd ~/.config/cower && exec makepkg -si }
+} && {
+  printf "\n\nInstalling pacaur\n\n" \
+  && git clone https://aur.archlinux.org/pacaur.git ~/.config/pacaur \
+  && { cd ~/.config/pacaur && exec makepkg -si }
+} && {
+  echo "\n\nStarting ssh service\n\n" \
+  && sudo systemctl start sshd.socket
+} && {
+  echo "\n\nGenerating ssh-keygen\n\n" \
+  && ssh-keygen -t rsa -b 4096
+} && {
+  echo "\n\nCopying ssh-key\n\n" \
+  && ssh-copy-id $USER@localhost
+} && {
+  echo "\n\nRemove repos\n\n" \
+  && rm -rf ~/.config/playbooks ~/.config/ansible-pacaur ~/.config/ansible-human-log
+} && {
+  echo "\n\nCloning ansible-pacaur repo\n\n" \
+  && git clone https://github.com/arperalta3/ansible-pacaur.git ~/.config/ansible-pacaur
+} && {
+  echo "\n\nCloning ansible-human-log repo\n\n" \
+  && git clone https://github.com/jinesh-choksi/ansible-human_log.git ~/.config/ansible-human-log
+} && {
+  echo "\n\nCloning playbooks repo\n\n" \
+  && git clone https://github.com/arperalta3/playbooks.git ~/.config/playbooks
+} && {
+  printf "\n\nLinking ansible library pacaur\n\n" \
+  && ln -s ~/.config/ansible-pacaur/pacaur ~/.config/playbooks/ansible-playbooks/library/pacaur
+} && {
+  printf "\n\nLinking ansible callback human log\n\n" \
+  && ln -s ~/.config/ansible-human-log/human_log.py ~/.config/playbooks/ansible-playbooks/callback_plugins/human_log.py
+}
