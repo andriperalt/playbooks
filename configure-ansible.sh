d35b217df6166f6e7b899665cc8c0dbee0c0e22d
Andrés Peralta <ar.peralta.3@gmail.com>
2017-07-31 16:19:35 -0500

    Maybe fix of problem bad format

--- configure-ansible.sh
+++ configure-ansible.sh
@@ -30,11 +30,11 @@ set -o errexit
   echo "\n\nInstalling cower for pacaur\n\n" \
   && git clone https://aur.archlinux.org/cower.git ~/.config/cower \
   && gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53 \
-  && { cd ~/.config/cower && exec makepkg -si }
+  && cd ~/.config/cower && exec makepkg -si
 } && {
   printf "\n\nInstalling pacaur\n\n" \
   && git clone https://aur.archlinux.org/pacaur.git ~/.config/pacaur \
-  && { cd ~/.config/pacaur && exec makepkg -si }
+  && cd ~/.config/pacaur && exec makepkg -si
 } && {
   echo "\n\nStarting ssh service\n\n" \
   && sudo systemctl start sshd.socket
